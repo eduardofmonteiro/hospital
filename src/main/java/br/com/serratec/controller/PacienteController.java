d7a10d2a28e285958d83fd42cf5462dd2565d1a1
Eduardo Felipe <44728582+eduardofmonteiro@users.noreply.github.com>
2024-10-11 16:47:00 -0300

    Final

--- src/main/java/br/com/serratec/controller/PacienteController.java
+++ src/main/java/br/com/serratec/controller/PacienteController.java
@@ -2,6 +2,8 @@ package br.com.serratec.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -10,8 +12,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+
 import br.com.serratec.entity.Paciente;
 import br.com.serratec.repository.PacienteRepository;
+import jakarta.validation.Valid;
 
 public class PacienteController {
 
@@ -29,6 +33,16 @@ public class PacienteController {
 	public List <Paciente> listar(){
 		return repository.findAll();
 	}
+	
+	@GetMapping("/{id}/consultas")
+    public Paciente listarPacienteComConsultas(@Valid @PathVariable Long id) {
+        Optional<Paciente> paciente = repository.findById(id);
+        if (paciente.isPresent()) {
+            return paciente.get();
+        } else {
+            throw new RuntimeException("Paciente não encontrado");
+        }
+    }
 	
 	@DeleteMapping("{id}")
     public ResponseEntity<Void> apagar(@PathVariable Long id) {
